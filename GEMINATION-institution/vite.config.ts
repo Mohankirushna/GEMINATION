3fe483674ade610a5894aa1b89f4ee97a158f17e
Arvind Nandigam <160626066+ArvindNandigam@users.noreply.github.com>
2026-03-03 06:11:20 +0530

    Update vite.config.ts

--- GEMINATION-institution/vite.config.ts
+++ GEMINATION-institution/vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig(({ mode }) => {
       hmr: true,
       proxy: {
         "/api": {
-          target: "http://localhost:8000",
+          target: process.env.VITE_API_URL || "http://localhost:8000",
           changeOrigin: true,
         },
       },
